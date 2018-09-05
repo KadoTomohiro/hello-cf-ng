33b34ed7876f4d8f2bc748e9c1a2155cee1b5bac
KadoTomohiro <corner0427@gmail.com>
2018-09-05 17:01:57 +0900

    サンプルアプリの追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CatComponent } from './cat/cat.component';
+import { DogComponent } from './dog/dog.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'cat', component: CatComponent},
+  {path: 'dog', component: DogComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CatComponent } from './cat/cat.component';
+import { DogComponent } from './dog/dog.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatComponent,
+    DogComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
